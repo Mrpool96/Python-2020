1a8f840754020bcf59894e3021e9e01e9852c6e7
Mrpool96 <53513296+Mrpool96@users.noreply.github.com>
2020-08-18 11:57:39 +0530

    using recursion in python by writing a factorial program

---
+++ .idea/Control_Statements/Recursion.py
@@ -0,0 +1,14 @@
+#Example of Factorial Using Resursion in Python
+#Finding A Factorial Of A Given Number
+def factorial(num):
+    """This function calls itself to find
+    the factorial of a number"""
+
+    if num == 1:
+        return 1
+    else:
+        return (num * factorial(num - 1))
+
+
+num = 5
+print("Factorial of", num, "is: ", factorial(num))
