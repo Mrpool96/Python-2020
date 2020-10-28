8f8708a3de185dd30cc750db1c749dd97546b7e0
Mrpool <sagarkarambelkar@gmail.com>
2020-10-28 12:11:19 +0530

    Get The Shape OF An Array

---
+++ Array.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr = np.array([[1,2,3,4,5,6],["Marvel","DC","Legendary","Dragon-ball","Pokemon","SonyMCU"]])
+print(arr.shape)
+#NumPy arrays have an attribute called shape that returns
+# a tuple with each index having the number of corresponding elements.
+
