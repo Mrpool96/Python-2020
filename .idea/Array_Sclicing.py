3005fec472363a57ece658889ad4a983a41d56c1
Mrpool <sagarkarambelkar@gmail.com>
2020-10-28 11:45:48 +0530

    Array_Slicing program using NumPy

--- .idea/Array_Sclicing.py
+++ .idea/Array_Sclicing.py
@@ -8,3 +8,13 @@ current_time = now.strftime("%H:%M:%S")
 print("Today's date is:- " ,today)
 print("Current time = " , current_time)
 
+arr = np.array(["Sagar",2,"Aniket",3,"Abhi",4])
+
+print(arr[1:5])
+
+#We pass slice instead of index like this: [start:end].
+#We can also define the step, like this: [start:end:step].
+#If we don't pass start its considered 0
+#If we don't pass end its considered length of array in that dimension
+#If we don't pass step its considered 1 #
+
