eee3e1f5970f9f8590ddb5ce9995e5c60c76a28b
Mrpool <sagarkarambelkar@gmail.com>
2020-10-30 13:05:22 +0530

    Creating a CSR Matrix from a ARRAY

---
+++ CSR_matrix.py
@@ -0,0 +1,7 @@
+import numpy as np
+from scipy.sparse import csc_matrix
+arr = np.array([0,0,0,0,0,1,1,0,2])
+
+arr = np.array([[0,0,0],[0,0,1],[1,0,2]])
+print(csc_matrix(arr).data)
+print(csc_matrix(arr))
