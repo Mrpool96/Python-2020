22621426472d5821a6e546b5f99b1610ae5430a5
Mrpool <sagarkarambelkar@gmail.com>
2020-10-30 11:52:51 +0530

    Creating a simple piechart with labels

--- Piechart.py
+++ Piechart.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 y = np.array([35,25,25,10,15])
+mylabels = ["one","two","three","four","five"]
 
-plt.pie(y)
+plt.pie(y , labels=mylabels)
 plt.show()
